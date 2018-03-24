14bf36e2a713e529333be7d2ab56419de95cf3b3
mc0222 <dakensin@gmail.com>
2018-03-24 16:17:03 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #curl -sSL https://zipkin.io/quickstart.sh | bash -s
 docker build . -t hubtea/zipkin
-docker push hubtea/zipkin
+docker push dakensin/zipkin
 kubectl apply -f deployment.yaml -n development
